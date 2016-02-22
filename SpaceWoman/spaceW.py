1b0ac03bddea8c96004bad8a0cc21852af97d361
Daniel <cranky30@hotmail.com>
2016-02-22 22:20:27 +0800

    coded the rocket and freighter functions

--- SpaceWoman/spaceW.py
+++ SpaceWoman/spaceW.py
@@ -20,4 +20,29 @@ def rocket_freighter(c):
 	if c.lower() == "rocket":
 		rocket()
 	elif c.lower() == spacefreighter:
-		space_freighter
+		space_freighter()
+		
+# String->String
+# Takes the player to mars on a rocket
+# def rocket(): "string
+# Tests
+def rocket():
+	print("It will take you 2 month to arrive on the moon")
+	print("you are the captain of the rocket and are able")
+	print("to make it go up, down, left, right\n")
+	print("There will be black holes and other obstacles along the way")
+	
+# None->string
+# the player takes a freighter to mars
+# def freighter(): string
+# tests
+def freighter():
+	print("You are a passenger of a freighter captained by someone else")
+	print("you can sit back and enjoy the sights and environment of the")
+	print("freighter.\n")
+	print("There will be many obstacles for you to over come.")
+	
+freighter()
+
+		
+		
