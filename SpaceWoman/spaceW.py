4b1fe7516367907f4cd1e0ebd0d4f2d7585245a5
Daniel <cranky30@hotmail.com>
2016-02-24 23:04:58 +0800

    worked on functions

--- SpaceWoman/spaceW.py
+++ SpaceWoman/spaceW.py
@@ -8,7 +8,8 @@ def intro():
 	print("An amateur detective and space traveler")
 	print("Your first mission is to travel to mars")
 	print("Do you want to take a rocket or spacefreighter\n")
-	choice = input("Rocket or SpaceFreighter?\n>")
+	choice = input("Rocket or Freighter?\n>")
+	rocket_freighter(choice)
 	# What does Hanna need to do on mars?
 
 # string->String
@@ -19,15 +20,16 @@ def intro():
 def rocket_freighter(c):
 	if c.lower() == "rocket":
 		rocket()
-	elif c.lower() == spacefreighter:
-		space_freighter()
+	elif c.lower() == "freighter":
+		freighter()
+	
 		
 # String->String
 # Takes the player to mars on a rocket
 # def rocket(): "string
 # Tests
 def rocket():
-	print("It will take you 2 month to arrive on the moon")
+	print("It will take you 2 months to arrive on the moon")
 	print("you are the captain of the rocket and are able")
 	print("to make it go up, down, left, right\n")
 	print("There will be black holes and other obstacles along the way")
@@ -42,7 +44,7 @@ def freighter():
 	print("freighter.\n")
 	print("There will be many obstacles for you to over come.")
 	
-freighter()
+intro()
 
 		
 		
